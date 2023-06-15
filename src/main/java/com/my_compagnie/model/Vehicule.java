499b72e90b3b9b2902281d0aec044a59abdad3ad
DevZenBook <julien.mangeat@mail-formateur.net>
2023-06-15 09:57:10 +0200

    ajout class vehicule

---
+++ src/main/java/com/my_compagnie/model/Vehicule.java
@@ -0,0 +1,10 @@
+package com.my_compagnie.model;
+import lombok.Getter;
+
+@Getter
+public class Vehicule {
+    private int id;
+    private String marque;
+    private String modele;
+    private String annee;
+}
